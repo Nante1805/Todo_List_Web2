c2b08d1eb3eee5ab673a71f4dde330d39d4cb04d
Nante1805 <hei.nantenaina.2@gmail.com>
2023-08-01 11:07:44 +0200

    init

---
+++ src/TodoList.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+
+function TodoList() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTodos([...todos, inputValue]);
+    setInputValue('');
+  };
+
+  const handleDelete = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  const handleUpdate = (index) => {
+    const newTodos = [...todos];
+    newTodos[index] = prompt('Enter new todo:');
+    setTodos(newTodos);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button type="submit">Add</button>
+      </form>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => handleDelete(index)}>Delete</button>
+            <button onClick={() => handleUpdate(index)}>Update</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TodoList;
